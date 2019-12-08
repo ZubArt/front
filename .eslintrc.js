9cdbc23d7f1478a2a9e69bc954ecb01b9de17f44
Artem Zubkov <artzub@gmail.com>
2019-12-08 20:00:25 +0700

    added rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': [0],
+    'func-names': ['error', 'always', { 'generators': 'never' }],
+    'import/prefer-default-export': [0],
+    'no-param-reassign': [0],
+    'arrow-parens': [0]
   },
   parserOptions: {
     parser: 'babel-eslint',
